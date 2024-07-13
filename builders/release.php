d08aecbc1bb990e5a9ee06b29a79bce83ea8d0de
gridorius <gridorius@yandex.ru>
2024-07-13 23:23:59 +0500

    hotfix release script

--- builders/release.php
+++ builders/release.php
@@ -5,7 +5,7 @@ use Builder\BuildManager;
 include __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 const WIN = false;
 // build ppm
-$outPath = __DIR__ . '/../';
+$outPath = realpath(__DIR__ . '/../ppm');
 $ppmPaths = [PPM_BUILD_DIRECTORY, $outPath];
 $assemblyPaths = [ASSEMBLY_BUILD_DIRECTORY, $outPath];
 $buildManager = new BuildManager();
